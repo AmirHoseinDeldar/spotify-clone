e07820eb53c307c0cda2b5a67ccdb034724ae784
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-06-18 02:38:35 +0330

    done show cover and music

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,13 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import Image from "next/image";
+import PageContent from "./components/PageContent";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-hidden">
       <Header>
@@ -25,7 +30,7 @@ export default function Home() {
             جدیدترین اهنگ ها
           </h1>
         </div>
-        <div>PageContent</div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
